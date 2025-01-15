dc233e732e7e92ad48ca1963a04314587450b006
baothaihcmut <thaibao22042k4@gmail.com>
2025-01-15 09:45:23 +0700

    libs module

--- libs/pkg/config/config.go
+++ libs/pkg/config/config.go
@@ -1,7 +1,25 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
 
-func Test() {
-	fmt.Println("Test")
+	"github.com/spf13/viper"
+)
+
+func LoadConfig(cfg interface{}, path string) (interface{}, error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Error reading config file:", err)
+		return nil, err
+	}
+
+	// Parse the config into a struct
+	if err := viper.Unmarshal(cfg); err != nil {
+		fmt.Println("Error unmarshalling config:", err)
+		return nil, err
+	}
+	return cfg, nil
 }
