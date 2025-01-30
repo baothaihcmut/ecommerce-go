118c475cb94faf503ec83c182ffeffd63b41d3d8
baothaihcmut <thaibao22042k4@gmail.com>
2025-01-30 20:09:36 +0700

    add transaction service lib

--- libs/pkg/postgres/db.go
+++ libs/pkg/postgres/db.go
@@ -4,6 +4,8 @@ import "database/sql"
 
 type TransactionService interface {
 	BeginTransaction() (*sql.Tx, error)
+	CommitTransaction(*sql.Tx) error
+	RollbackTransaction(*sql.Tx) error
 }
 
 type PostgresTransactionService struct {
@@ -14,6 +16,14 @@ func (p *PostgresTransactionService) BeginTransaction() (*sql.Tx, error) {
 	return p.db.Begin()
 }
 
+func (p *PostgresTransactionService) CommitTransaction(tx *sql.Tx) error {
+	return tx.Commit()
+}
+
+func (p *PostgresTransactionService) RollbackTransaction(tx *sql.Tx) error {
+	return tx.Rollback()
+}
+
 func NewPostgresTransactionService(db *sql.DB) TransactionService {
-	return &PostgresTransactionService{}
+	return &PostgresTransactionService{db: db}
 }
