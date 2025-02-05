c88a96e34cf5992b9450fab80d4076a6a8ca0c08
baothaihcmut <thaibao22042k4@gmail.com>
2025-02-05 15:44:01 +0700

    libs/done span lib

--- libs/go.mod
+++ libs/go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/spf13/viper v1.19.0
+	go.opentelemetry.io/otel v1.34.0
+	go.opentelemetry.io/otel/trace v1.34.0
 	go.uber.org/zap v1.27.0
 	google.golang.org/protobuf v1.35.1
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
@@ -15,14 +17,17 @@ require (
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/klauspost/compress v1.17.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
+	go.opentelemetry.io/otel/sdk v1.34.0 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
 
 require (
@@ -42,7 +47,7 @@ require (
 	go.mongodb.org/mongo-driver v1.17.2
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 	google.golang.org/grpc v1.69.4
 	gopkg.in/ini.v1 v1.67.0 // indirect
---
+++ libs/pkg/trace/span.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func setSpanAttributes(span trace.Span, attributes map[string]interface{}) {
+	for key, val := range attributes {
+		switch v := val.(type) {
+		case string:
+			span.SetAttributes(attribute.String(key, v))
+		case []string:
+			span.SetAttributes(attribute.StringSlice(key, v))
+		case int:
+			span.SetAttributes(attribute.Int(key, v))
+		case []int:
+			span.SetAttributes(attribute.IntSlice(key, v))
+		case float64:
+			span.SetAttributes(attribute.Float64(key, v))
+		case []float64:
+			span.SetAttributes(attribute.Float64Slice(key, v))
+		case bool:
+			span.SetAttributes(attribute.Bool(key, v))
+		case []bool:
+			span.SetAttributes(attribute.BoolSlice(key, v))
+		default:
+			// Handle unknown types
+			// Optionally log or ignore unsupported attribute types
+		}
+	}
+}
+
+func StartSpan(ctx context.Context, tracer trace.Tracer, name string, attributes map[string]interface{}) (context.Context, trace.Span) {
+	ctx, span := tracer.Start(ctx, name)
+	setSpanAttributes(span, attributes)
+	return ctx, span
+}
+
+func SetSpanAttribute(span trace.Span, attributes map[string]interface{}) {
+	setSpanAttributes(span, attributes)
+}
+
+func EndSpan(span trace.Span, err error, attributes map[string]interface{}) {
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+	}
+	setSpanAttributes(span, attributes)
+	span.End()
+}
